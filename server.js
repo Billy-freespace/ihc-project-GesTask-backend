68eaa30b11ae46889acaf2cf264bcb57e105c707
Billy-freespace <billy.freespace@gmail.com>
2023-06-04 11:01:14 -0500

    Implemented basic server with mongodb connection

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connnectDB from "./mongodb/connect.js";
+
+dotenv.config();
+const port = process.env.PORT;
+
+// Settings
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+// Middelwares
+
+// Routes
+app.get("/", (req, res) => {
+  res.send({ message: "Hello from backend!" });
+});
+
+// Starting the server
+const startServer = async () => {
+  try {
+    await connnectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server started on port http://localhost:${port}`)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+await startServer();
